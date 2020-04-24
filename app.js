5a8d669f6665705d12973a5056ede20c0d87d12f
Phạm Ngọc Vinh <vinhngocphamhp@gmail.com>
2020-04-24 10:52:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/client/build"));
 app.use(bodyParser.urlencoded({extended: true,}));
 app.use(bodyParser.json());
 app.use("/", routes);
-app.use('*', express.static(buildPath));
+app.use('*', express.static(buildPath));ddd
 
 var server = http.createServer(app);
 server.listen(app.get("port"), () =>{
